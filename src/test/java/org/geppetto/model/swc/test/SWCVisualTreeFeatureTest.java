9d2058db5ea41206d2aee3ce2b29a0d735cb3c19
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2015-09-09 12:12:59 +0100

    Dev branch

--- src/test/java/org/geppetto/model/swc/test/SWCVisualTreeFeatureTest.java
+++ src/test/java/org/geppetto/model/swc/test/SWCVisualTreeFeatureTest.java
@@ -68,6 +68,10 @@ public class SWCVisualTreeFeatureTest
 		ServicesRegistry.registerModelFormat("SWC");
 	}
 
+	/**
+	 * @throws ModelInterpreterException
+	 * @throws SWCException
+	 */
 	@Test
 	public void testVisualTreeFeature() throws ModelInterpreterException, SWCException
 	{
